0b517efe5fe1d7c90ff9f6b63b7d1dbaa86a62d1
RTU MIREA <mirea@mirea.ru>
2019-10-15 15:54:44 +0300

    X3

---
+++ Assets/Resources/BallControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float speed;
+
+    public float jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Time.deltaTime * speed * Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Time.deltaTime * speed * Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Time.deltaTime * speed * Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Time.deltaTime * speed * Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.Space) && 
+            Physics.Raycast(transform.position, Vector3.down, 0.55f))
+        {
+            rb.AddForce(jump * Vector3.up);
+        }
+    }
+}
